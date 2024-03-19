cab9122816651365598f35a1c0659a8e8ee9649c
EstefanoC <estefano.jesus6@gmail.com>
2024-03-18 21:47:16 -0400

    Add Logo and Logic correctly of theme in the Styles.scss

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 
 // Dependencies
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatGridListModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  getTheme() {
+    if (typeof document !== 'undefined') {
+      return document.body.classList.contains('dark-theme');
+    }
+
+    return false;
+  }
+
+  toggleTheme() {
+    // add light-theme or dark-theme class to body
+    if (document.body.classList.contains('dark-theme')) {
+      document.body.classList.remove('dark-theme');
+      document.body.classList.add('light-theme');
+    } else {
+      document.body.classList.remove('light-theme');
+      document.body.classList.add('dark-theme');
+    }
+  }
+}
