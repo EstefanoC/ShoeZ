5bfaca43f23508f7b66293496f9ac6c8faca1c40
EstefanoC <estefano.jesus6@gmail.com>
2024-04-17 19:52:37 -0400

    Fixed the animation of 360 on images

--- src/app/shared/components/single-product/image360/image360.component.ts
+++ src/app/shared/components/single-product/image360/image360.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 // Dependencies
@@ -10,7 +11,7 @@ import { Product } from '../../../../core/models/product.interface';
 @Component({
   selector: 'app-image360',
   standalone: true,
-  imports: [MatSliderModule, FormsModule],
+  imports: [MatSliderModule, FormsModule, CommonModule],
   templateUrl: './image360.component.html',
   styleUrl: './image360.component.sass',
 })
@@ -19,25 +20,14 @@ export class Image360Component {
   @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
   context!: CanvasRenderingContext2D;
   value: number = 1;
-  images: Array<any> = [];
-
-  getImage(path: string): string {
-    const index: number = path.indexOf('.jpg');
-    const currentlyValue: string = path.slice(index - 5, index);
-    let number: string = this.value.toString().padStart(2, '00');
-
-    return path.replace(currentlyValue, `img${number}`);
-  }
-
-  ngOnInit() {
-    this.context = this.canvas.nativeElement.getContext('2d')!;
-    let image = new Image();
-    image.src = this.imageData.path;
-  }
+  disabledSlider: boolean = true;
+  images: Array<CanvasImageSource> = [];
 
   ngAfterViewInit() {
     // Loop to put in a canvas the images
-    for (let i = 0; i < 36; i++) {
+    for (let i = 0; i < 37; i++) {
+      if (i === 0) this.images[0] = new Image();
+
       const index: number = this.imageData.path.indexOf('.jpg');
       const currentlyValue: string = this.imageData.path.slice(
         index - 5,
@@ -46,20 +36,63 @@ export class Image360Component {
       let number: string = i.toString().padStart(2, '00');
       const url = this.imageData.path.replace(currentlyValue, `img${number}`);
 
-      this.context = this.canvas.nativeElement.getContext('2d')!;
       let image = new Image();
       image.src = url;
 
       // Put the image after load
       image.addEventListener('load', () => {
-        this.context.drawImage(
-          image,
-          0,
-          0,
-          this.canvas.nativeElement.width,
-          this.canvas.nativeElement.height
-        );
+        this.images[i] = image;
+
+        this.images.length === 37 && this.animationSlider(); // Call initial animation
+
+        this.handleSlider(image);
       });
+    }
+  }
+
+  // Handle Canvas image
+  handleSlider(image: CanvasImageSource) {
+    this.context = this.canvas.nativeElement.getContext('2d')!;
+
+    this.context.drawImage(
+      image,
+      0,
+      0,
+      this.canvas.nativeElement.width,
+      this.canvas.nativeElement.height
+    );
+  }
+
+  // Change canvas when change slide value
+  ngAfterViewChecked() {
+    if (this.images.length === 37) {
+      this.handleSlider(this.images[this.value]);
+    }
+  }
+
+  // Animation to Slider on initial load
+  animationSlider() {
+    let time: number[] = [];
+
+    // First Animation in a Promise
+    for (let i = 0; i < this.images.length; i++) {
+      time.push(i * 50);
+
+      setTimeout(() => {
+        this.value = i;
+
+        // Call the second Animation
+        if (i === 35) {
+          for (let n = i; n > 0; n--) {
+            let times = [...time].reverse();
+
+            setTimeout(() => {
+              this.value = n;
+              if (n === 1) this.disabledSlider = false;
+            }, times[n]);
+          }
+        }
+      }, time[i]);
     }
   }
 }
