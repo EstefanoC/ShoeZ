0554e42638fe069ef1e69b1c976cc5fe40694e5b
EstefanoC <estefano.jesus6@gmail.com>
2024-07-12 16:04:42 -0400

    Create gripd of product in shop page

--- src/app/shared/components/shop/shop-grid/shop-grid.component.ts
+++ src/app/shared/components/shop/shop-grid/shop-grid.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+// Dependencies
+import { MatGridListModule } from '@angular/material/grid-list';
+
+// Components
+import { CardComponent } from '../../product-card/card/card.component';
+
+// Core
+import { Product } from '../../../../core/models/product.interface';
+import { AllProducts } from '../../../../core/defaultValues/product';
+
 @Component({
   selector: 'app-shop-grid',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, CardComponent],
   templateUrl: './shop-grid.component.html',
-  styleUrl: './shop-grid.component.sass'
+  styleUrl: './shop-grid.component.sass',
 })
 export class ShopGridComponent {
-
+  data: Product[] = AllProducts;
 }
